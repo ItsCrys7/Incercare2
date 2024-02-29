ed6d683c7743b80a29f5a6c41fa7b22ea72b127e
Crys <cristian.toanca@emanuel.ro>
2024-02-29 11:05:53 +0200

    index si altele

---
+++ script-module.js
@@ -0,0 +1,7 @@
+console.log("Salut din consola!")
+    
+        let counter = 0;
+        export function count(){
+            counter++;
+            console.log("Salutare din functie.", counter);
+        }
